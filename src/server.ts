f8beb1e06298a56283e42de80e6fd8e432005eae
kim pham <kimpham54@gmail.com>
2025-10-01 07:57:40 +0200

    update server settings for nginx

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -7,13 +8,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+
+const BASE_PATH = process.env.BASE_PATH || "";
+const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(BASE_PATH, express.static(path.join(__dirname, "../public")));
 
 // Routes
-app.use("/upload", uploadRouter);
+app.use(`${BASE_PATH}/upload`, uploadRouter);
+
+app.get('/', (req, res) => {
+  res.redirect(BASE_PATH);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
